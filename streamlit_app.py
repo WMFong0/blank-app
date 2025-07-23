1e76a429a2b1b2c9178c465972d257dd83104dda
WMFong0 <68149951+WMFong0@users.noreply.github.com>
2025-07-23 08:26:23 +0000

    Pure testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,8 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+test = st.text_input("Movie title", "Life of Brian")
+st.write("The current movie title is", test)
+st.title("A Movie of ", test)
