f79873d36248c7d8f1944044db269173a49f0f04
WMFong0 <68149951+WMFong0@users.noreply.github.com>
2025-07-24 03:23:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,9 +24,7 @@ if not District:
   st.header("Please provide your current district using the Input Panel. Thanks.")
 else:
   current_datetime = datetime.datetime.now().astimezone(datetime.timezone(datetime.timedelta(hours=8))); # Enforce Hong Kong Timezone
-  st.write("Here are the information we gathered from HKO
-  ")
-  st.markdown(current_datetime.strftime('Today is %Y/%m/%d. <br>Current Hong Kong Time is %H:%M:%S<br>'), unsafe_allow_html = True)
+  st.markdown("Here are the information we gathered from HKO<br><br>current_datetime.strftime('Today is %Y/%m/%d. <br>Current Hong Kong Time is %H:%M:%S<br>'), unsafe_allow_html = True)
 
 '''
 tab1, tab2 = st.tabs(["Input Page", "Result Page"])
